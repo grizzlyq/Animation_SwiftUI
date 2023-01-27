951313854450dc18e9a7638f85b0ec39a707bbc0
grizzlyq <pechenika73@yandex.ru>
2023-01-27 16:45:46 +0300

    Initial Commit

---
+++ Animation_SwiftUI/Animation_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_SwiftUIApp.swift
+//  Animation_SwiftUI
+//
+//  Created by Дмитрий on 27.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Animation_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
