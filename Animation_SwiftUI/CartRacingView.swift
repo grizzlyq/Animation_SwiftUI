b8cc16db41b97e4076f60d367cd50b7929d80a5c
grizzlyq <pechenika73@yandex.ru>
2023-01-28 20:59:51 +0300

    some copies

---
+++ Animation_SwiftUI/CartRacingView.swift
@@ -0,0 +1,95 @@
+//
+//  CartRacingView.swift
+//  Animation_SwiftUI
+//
+//  Created by Дмитрий on 27.01.2023.
+//
+
+import SwiftUI
+
+struct CartRacingView: View {
+    @State private var start = false
+    
+    var body: some View {
+        HStack {
+            VStack(spacing: 10) {
+                CartView(
+                    start: start,
+                    title: "Default",
+                    color: .red,
+                    animation: .default
+                )
+                CartView(
+                    start: start,
+                    title: "EaseOut",
+                    color: .green,
+                    animation: .easeOut(duration: 0.7)
+                )
+                CartView(
+                    start: start,
+                    title: "EaseIn",
+                    color: .yellow,
+                    animation: .easeIn(duration: 0.7)
+                )
+                CartView(
+                    start: start,
+                    title: "EaseInOut",
+                    color: .blue,
+                    animation: .easeInOut(duration: 0.7).repeatCount(3, autoreverses: true)
+                )
+                
+                CartView(
+                    start: start,
+                    title: "InterpolatingSpring",
+                    color: .brown,
+                    animation: .interpolatingSpring(
+                        mass: 1,
+                        stiffness: 100,
+                        damping: 10,
+                        initialVelocity: 0
+                    ).delay(0.2)
+                )
+                CartView(
+                    start: start,
+                    title: "Spring",
+                    color: .orange,
+                    animation: .spring(
+                        response: 0.55,
+                        dampingFraction: 0.45
+                    ).speed(2)
+                )
+                
+            }
+            Spacer()
+        }
+        .onTapGesture {
+            start.toggle()
+        }
+    }
+}
+
+struct CartRacingView_Previews: PreviewProvider {
+    static var previews: some View {
+        CartRacingView()
+            .previewInterfaceOrientation(.landscapeLeft)
+    }
+}
+
+struct CartView: View {
+    let start: Bool
+    let title: String
+    let color: Color
+    let animation: Animation
+    
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "cart.fill")
+                .font(.title)
+                .foregroundColor(color)
+                .offset(x: start ? UIScreen.main.bounds.width - 200 : 0)
+                .animation(animation, value: start)
+            Text(title)
+        }
+    }
+}
